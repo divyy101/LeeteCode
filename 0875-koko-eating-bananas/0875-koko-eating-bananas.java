95c96adb8c915a9275f4992cdd10891f4e548584
Divyansh Singh <divyanshsingh74178@gmail.com>
2026-07-08 12:07:52 +0530

    Time: 8 ms (69.31%), Space: 47.9 MB (59.01%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,33 +1,49 @@
-class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        
+
+ class Solution {
+
+    public static int minEatingSpeed(int[] piles, int h) {
+
         int low = 1;
         int high = 0;
-        
-        for (int p : piles) {
-            high = Math.max(high, p);
+
+        for (int pile : piles) {
+            high = Math.max(high, pile);
         }
-        
+
         while (low < high) {
+
             int mid = low + (high - low) / 2;
-            
-            if (canFinish(piles, h, mid)) {
-                high = mid;   
+
+            int hours = 0;
+
+            for (int pile : piles) {
+                hours += (pile + mid - 1) / mid;
+            }
+
+            if (hours <= h) {
+                high = mid;
             } else {
-                low = mid + 1;  
+                low = mid + 1;
             }
         }
-        
+
         return low;
     }
-    
-    private boolean canFinish(int[] piles, int h, int k) {
-        int hours = 0;
-        
-        for (int p : piles) {
-            hours += (p + k - 1) / k;  
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] piles = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            piles[i] = sc.nextInt();
         }
-        
-        return hours <= h;
+
+        int h = sc.nextInt();
+
+        System.out.println(minEatingSpeed(piles, h));
     }
 }
