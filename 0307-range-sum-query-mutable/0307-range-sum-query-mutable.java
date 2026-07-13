440305de0f0f752336b939ea477bead5ae10b645
Divyansh Singh <divyanshsingh74178@gmail.com>
2026-07-13 16:19:25 +0530

    Time: 73 ms (87.3%), Space: 134.7 MB (22.35%) - LeetHub

---
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -0,0 +1,45 @@
+class NumArray {
+    int[] ft;
+    int[] arr;
+
+    public NumArray(int[] nums) {
+        arr = nums;              // no clone
+        ft = new int[nums.length + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            updateFT(i + 1, nums[i]);
+        }
+    }
+
+    void updateFT(int l, int r) {
+        int i = l;
+
+        while (i < ft.length) {
+            ft[i] = ft[i] + r;
+            i = i + (i & -i);
+        }
+    }
+
+    int rangeSum(int r) {
+        int i = r;
+        int sum = 0;
+
+        while (i > 0) {
+            sum = sum + ft[i];
+            i = i - (i & -i);
+        }
+
+        return sum;
+    }
+
+    public void update(int index, int val) {
+        int difference = val - arr[index];
+        arr[index] = val;
+
+        updateFT(index + 1, difference);
+    }
+
+    public int sumRange(int left, int right) {
+        return rangeSum(right + 1) - rangeSum(left);
+    }
+}
